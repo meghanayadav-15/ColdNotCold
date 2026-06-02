e083b8e7cd0ba4952705e41f866fcc8d61b411c0
meghanayadav-15 <meghanayadav.seattle@gmail.com>
2026-06-01 21:50:52 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'ColdNotCold',
+  description: 'Smart LinkedIn outreach, on autopilot.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
